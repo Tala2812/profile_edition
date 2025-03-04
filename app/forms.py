6d1322daf0c58cbd214177e222adb71ebd9b81d6
Tala2812 <guskovatala@gmail.com>
2025-03-04 15:27:41 +0300

    styles.css added
    Добавлены стили

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
+
 class EditProfileForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    email = StringField('Почта', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password2 = PasswordField('Повторить пароль', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Имя пользователя',
+                           validators=[DataRequired(message="Поле 'Имя пользователя' обязательно для заполнения.")])
+    email = StringField('Почта', validators=[DataRequired(message="Поле 'Email' обязательно для заполнения."),
+                                             Email(message="Введите корректный email.")])
+    password = PasswordField('Пароль', validators=[DataRequired(message="Поле 'Пароль' обязательно для заполнения.")])
+    password2 = PasswordField('Повторить пароль',
+                              validators=[DataRequired(message="Поле 'Повторите пароль' обязательно для заполнения."),
+                                          EqualTo('password', message="Пароли должны совпадать.")])
     submit = SubmitField('Сохранить изменения')
+
+
